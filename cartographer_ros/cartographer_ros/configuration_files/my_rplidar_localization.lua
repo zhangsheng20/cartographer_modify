ca748837d15d6fe27552ad089576147764d337b5
zhangsheng20 <969653196@qq.com>
2020-07-27 23:46:33 +0800

    22222

--- cartographer_ros/cartographer_ros/configuration_files/my_rplidar_localization.lua
+++ cartographer_ros/cartographer_ros/configuration_files/my_rplidar_localization.lua
@@ -18,6 +18,12 @@
 include "my_rplidar.lua"
 
 TRAJECTORY_BUILDER.pure_localization = true
-POSE_GRAPH.optimize_every_n_nodes = 20
+POSE_GRAPH.optimize_every_n_nodes = 1
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight=20
+POSE_GRAPH.constraint_builder.sampling_ratio=3e-1
+
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds=5
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters=0.2
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.5)
 
 return options
